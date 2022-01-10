d5ba619321726f5a57681a25af38c96e5dd49af9
Sarthak Agrawal <agrawal.sarthak2599@gmail.com>
2022-01-10 13:09:17 +0530

    Use the useState hook one time

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState} from 'react';
 function App() {
+  const [email, setEmail] = useState ('');
+
+  function handleChange (e) {
+    setEmail(e.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App' style={{padding:10}}>
+      <input 
+        value={email}
+        placeholder='abc@gmail.com'
+        onChange={handleChange}
+      />
+      <p>Email: {email} </p>
     </div>
   );
 }
-
 export default App;
